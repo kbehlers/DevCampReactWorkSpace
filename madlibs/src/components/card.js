dda87efe6886547cc3a11ce874945730efdd7c5b
kbehlers <kemais.ehlers@gmail.com>
2018-10-10 12:05:47 -0600

    two way binding with inputs on card

--- madlibs/src/components/card.js
+++ madlibs/src/components/card.js
@@ -3,11 +3,30 @@ import React, {Component} from 'react';
 import Input from './input'
 
 class Card extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            color: 'blue',
+            pluralNoun: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        console.log(event.target.value)
+        this.setState({color:event.target.value})
+    }
     render() {
         return (
             <div className="card">
+            <h1>{this.state.color}</h1>
+
                 { Input('Number', 'Noun' ) }
                 { Input('Items', 'Plural Noun' ) }
+                <input onChange={(event) => this.handleInputChange(event)}/>
             </div>
         )
     }
