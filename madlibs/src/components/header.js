09953dba7584fd876c0088268f343adbc3aadcf0
kbehlers <kemais.ehlers@gmail.com>
2018-10-11 09:52:54 -0600

    added header h1 & p styles

--- madlibs/src/components/header.js
+++ madlibs/src/components/header.js
@@ -4,6 +4,8 @@ import React from 'react';
 const Header = (title, content) => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>{title}</h1>
             <p>{content}</p>
         </div>
