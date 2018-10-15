1535aaba76fd602d26acec316f78ebc4a6311a47
kbehlers <kemais.ehlers@gmail.com>
2018-10-15 13:59:16 -0600

    styled large text, change date button addeded

--- birthdayCountdown/src/components/app.js
+++ birthdayCountdown/src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 import Picker from './picker';
-import Button from "./button"
-import Clock from "./clock"
+import Button from "./button";
+import Clock from "./clock";
+import ChangeDate from "./changeDate";
+import LargeText from './largeText';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false
+    }
+  }
+
+  renderItems = function() {
+    if(this.state.active) {
+      return [
+        <Clock/>,
+        ChangeDate("Change Date", () => this.setState( {active:false} )),
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining until your 28th birthday</label>
+      ]
+    } else { 
+      return [
+        <Picker />,
+        Button ('Generate Countdown', () => this.setState({active: true}))
+      ]
+    }
+  }.bind(this)
   render() {
     return (
       <div className='grid'>
@@ -15,9 +39,7 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
 
-        <Picker />
-        { Button ('Generate Countdown') }
-        <Clock />
+        { this.renderItems() }
       </div>
     );
   }
