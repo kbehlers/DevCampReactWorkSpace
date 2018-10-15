80bebe20fe08bf29657d082ffd14617e4e49b6ef
kbehlers <kemais.ehlers@gmail.com>
2018-10-15 11:57:59 -0600

    styled clock with CSS grid

--- birthdayCountdown/src/components/clock.js
+++ birthdayCountdown/src/components/clock.js
@@ -5,19 +5,19 @@ class Clock extends Component {
             <div className="clock">
             
                 <div className="clock__days">
-                    <label htmlFor="" className="clock__title">DAYS</label>
-                    <label htmlFor="" className="clock__amount"></label>
+                    <label htmlFor="" className="clock__title clock__box">DAYS</label>
+                    <label htmlFor="" className="clock__amount">20</label>
                 </div>
                 <div className="clock__hours">
-                    <label htmlFor="" className="clock__title">HOURS</label>
+                    <label htmlFor="" className="clock__title clock__box">HRS</label>
                     <label htmlFor="" className="clock__amount"></label>
                 </div>
                 <div className="clock__minutes">
-                    <label htmlFor="" className="clock__title">MINUTES</label>
+                    <label htmlFor="" className="clock__title clock__box">MINS</label>
                     <label htmlFor="" className="clock__amount"></label>
                 </div>
                 <div className="clock__seconds">
-                    <label htmlFor="" className="clock__title">SECONDS</label>
+                    <label htmlFor="" className="clock__title clock__box">SECS</label>
                     <label htmlFor="" className="clock__amount"></label>
                 </div>
             </div>
