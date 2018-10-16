57a828b7b016e1ea3dd37e06b8e1d631a09f6c33
kbehlers <kemais.ehlers@gmail.com>
2018-10-16 16:00:27 -0600

    added arrow component

--- course-scheduler/src/components/library/libraryCourse.js
+++ course-scheduler/src/components/library/libraryCourse.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import Icon from "../icon";
+import Arrow from "../arrow";
+
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <label htmlFor="" className="library-course__title">Problem Solving</label>
-                {/* TODO: ICON COMPONENT  */}
-                {/* TODO: ARROW COMPONENT */}
+                <div class="library-course__title-arrow">
+                    <label htmlFor="" className="library-course__title">Problem Solving</label>
+                    {Icon("fas fa-check", 'library-course__icon')}
+
+                </div>
+                <Arrow className="library-course__arrow" />
                 {/* TODO: ACTION BUTTON COMPONENT */}
+                {Icon("fas fa-plus-circle", "library-course__action")}
                 <div className="library-course__description">
                     <label htmlFor="">Course Description</label>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magnam quis repellendus officiis minus ut ipsam mollitia quia non fugiat.</p>
