f1fda2d61010e96a39ea5a79d63dceac598f252b
kbehlers <kemais.ehlers@gmail.com>
2018-10-16 16:12:15 -0600

    added plus icon

--- course-scheduler/src/components/library/libraryCourse.js
+++ course-scheduler/src/components/library/libraryCourse.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Icon from "../icon";
 import Arrow from "../arrow";
+import Action from "../action";
 
 class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <div class="library-course__title-arrow">
+                <div class="library-course__title-check">
                     <label htmlFor="" className="library-course__title">Problem Solving</label>
                     {Icon("fas fa-check", 'library-course__icon')}
 
                 </div>
                 <Arrow className="library-course__arrow" />
-                {/* TODO: ACTION BUTTON COMPONENT */}
-                {Icon("fas fa-plus-circle", "library-course__action")}
+                <Action className="library-course__action"/>
                 <div className="library-course__description">
                     <label htmlFor="">Course Description</label>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magnam quis repellendus officiis minus ut ipsam mollitia quia non fugiat.</p>
