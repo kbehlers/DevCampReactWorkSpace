b229f47883121cf5c65da515552e5b33d41a0a37
kbehlers <kemais.ehlers@gmail.com>
2018-10-17 15:46:30 -0600

    styled add remove button

--- course-scheduler/src/components/action.js
+++ course-scheduler/src/components/action.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Icon from './icon';
 class Action extends Component {
+
+    handleAction = function () {
+        this.props.onClick();
+        // document.getElementById('action').classList.add
+    }.bind(this);
+
     render() {
         return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                {Icon("fas fa-plus-circle")}
+            <a id='action' onClick={() => this.handleAction()} className={`${this.props.className} action`}>
+                {/* {Icon("fas fa-plus-circle")} */}
                 {/* <i class="fas fa-times-circle"></i> */}
-            </button>
+            </a>
+            // <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+            //     {Icon("fas fa-plus-circle")}
+            //     {/* <i class="fas fa-times-circle"></i> */}
+            // </button>
         )
     }
 }
