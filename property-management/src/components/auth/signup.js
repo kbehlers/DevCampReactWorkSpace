c5716361742862ed84589f9402b7098d6129e90c
kbehlers <kemais.ehlers@gmail.com>
2018-10-19 15:11:19 -0600

    Setup auth routes and headewrapper component

---
+++ property-management/src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class Signup extends Component {
+    render() {
+        return (
+            <div className='sign-up'>
+                ...Signup
+            
+            </div>
+        )
+    }
+}
+export default Signup;
