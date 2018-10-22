a63ae760f9d9dfe1b9a706c535cdf70a876c3455
kbehlers <kemais.ehlers@gmail.com>
2018-10-22 15:49:48 -0600

    handle signup post request

--- property-management/src/actions/auth.js
+++ property-management/src/actions/auth.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 import { ROOT_URL } from '../config';
+import {
+    AUTHENTICATE_USER
+} from './types';
 
 export function signUp(fields, success) {
     console.log(fields);
+    
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
         .then(response => {
-
+            console.log(response);
+            success();
         })
         .catch(err => {
             if(err) {
