0cd9d745bb9f63368613ac6bb071a010247465d7
kbehlers <kemais.ehlers@gmail.com>
2018-10-22 13:00:56 -0600

    added sign up form

--- property-management/src/components/auth/signup.js
+++ property-management/src/components/auth/signup.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
+import SignupForm from './signupForm';
 class Signup extends Component {
     render() {
         return (
             <div className='sign-up'>
-                ...Signup
-            
+                <SignupForm/>         
             </div>
         )
     }
