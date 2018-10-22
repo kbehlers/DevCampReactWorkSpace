30befb2cb9d32ddf51e3624d91958e56d7a97ec1
kbehlers <kemais.ehlers@gmail.com>
2018-10-22 14:00:05 -0600

    installed redux-thunk and axios, built auth actions

--- property-management/src/components/auth/signupForm.js
+++ property-management/src/components/auth/signupForm.js
@@ -6,15 +6,18 @@ import TextLink from '../textLink';
 
 class SignupForm extends Component {
     render() {
+
+        const { handleSubmit } = this.props;
+
         return (
-            <form action="" className='sign-up-form'>
+            <form onSubmit={handleSubmit} className='sign-up-form'>
                 <FormTitle className='sign-up-form__title' text='New User'></FormTitle>
                 <Field className='sign-up-form__fullname' placeholder='Enter Your Full Name' component={FormInput} name='fullname' type='text' title="Full Name" />
                 <Field className='sign-up-form__unit' placeholder='Enter Unit #' component={FormInput} name='unit' type='text' title="Unit #" />
                 <Field className='sign-up-form__email' placeholder='Enter Email' component={FormInput} name='email' type='email' title="Email" />
                 <Field className='sign-up-form__password' placeholder='Enter Password' component={FormInput} name='password' type='password' title="Password" />
                 <Field className='sign-up-form__create-account' component={FormButton} name='createaccount' type='submit' title="Create Account" />
-                <div class="sign-up-form__text-links">
+                <div className="sign-up-form__text-links">
                     <TextLink to="/signin" text='Already Registered? Login'/>
                 </div>
             </form>
