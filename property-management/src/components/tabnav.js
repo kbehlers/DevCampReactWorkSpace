d8abefa132175464f9a476fc397e2f339089135b
kbehlers <kemais.ehlers@gmail.com>
2018-10-23 15:10:24 -0600

    added click handler callback to tabnav

--- property-management/src/components/tabnav.js
+++ property-management/src/components/tabnav.js
@@ -1,20 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class TabNav extends Component {
     render() {
-        return(
+        return (
             <div className='tab-nav'>
                 <div className="tab-nav__tabs">
-                    { 
+                    {
                         this.props.tabs.map((tab, index) => {
-                            return <a className="tab-nav__tab">{tab.title}</a>
-                    })
+                            return <a key={index} onClick={() => this.props.handleClick(tab.title)} className="tab-nav__tab">{tab.title}</a>
+                        })
                     }
                 </div>
-                <div>Requests or newsletter content</div>
+
+                {
+                    this.props.tabs.map((tab, index) => {
+                        if (tab.active) {
+                            return (
+                                <div key={index} className='tab-nav__component'>
+                                    {tab.component}
+                                </div>
+                            )
+                        }
+                    })
+                }
             </div>
         )
     }
-  }
+}
 
 export default TabNav;
