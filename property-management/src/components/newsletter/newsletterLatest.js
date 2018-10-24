3b5b4648ff1fd0268adff379f10f75f313824f7e
kbehlers <kemais.ehlers@gmail.com>
2018-10-24 13:14:00 -0600

    built newsletter latest component and grid and refactored grid

---
+++ property-management/src/components/newsletter/newsletterLatest.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class NewsletterLatest extends Component {
+    render() {
+        return (
+            <div className='newsletter-latest'>
+                <h1 className='newsletter-latest__title'>Title goes here</h1>
+                <img src="http://via.placeholder.com/960x258" alt="" className="newsletter-latest__image"/>
+                <div className="newsletter-latest__body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a!</p>
+
+                </div>
+            </div>
+        )
+    }
+}
+export default NewsletterLatest;
