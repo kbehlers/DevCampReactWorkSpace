43e141e16013df06db44388cdbed1c79ca203609
kbehlers <kemais.ehlers@gmail.com>
2018-10-24 13:37:47 -0600

    mapped over JSON object for the latest newsletter component

--- property-management/src/components/newsletter/newsletterGrid.js
+++ property-management/src/components/newsletter/newsletterGrid.js
@@ -6,11 +6,18 @@ import NewsletterLatest from './newsletterLatest';
 class NewsletterGrid extends Component {
 
     render() {
+        const latest = {
+            _id: '115',
+            title: 'Happy Holidays',
+            body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsum cupiditate, aperiam iure molestias exercitationem accusantium cumque sunt expedita quibusdam totam. Enim doloremque pariatur vero dignissimos, repellat cum atque eos!`,
+            date: new Date(),
+            imageUrl: 'http://via.placeholder.com/960x258'
+        }
         return (
             <div className='newsletter-grid'>
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
-                <NewsletterLatest/>
+                <NewsletterLatest {...latest}/>
             </div>
         )
     }
--- property-management/src/components/newsletter/newsletterLatest.js
+++ property-management/src/components/newsletter/newsletterLatest.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 class NewsletterLatest extends Component {
     render() {
+        const { title, imageUrl, body } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'>Title goes here</h1>
-                <img src="http://via.placeholder.com/960x258" alt="" className="newsletter-latest__image"/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img src={imageUrl} alt="" className="newsletter-latest__image"/>
                 <div className="newsletter-latest__body">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsum cupiditate, aperiam iure molestias exercitationem accusantium cumque sunt expedita quibusdam totam. Enim doloremque pariatur vero dignissimos, repellat cum atque eos!</p>
+                    <p>
+                        {body}
+                    </p>
 
                 </div>
             </div>
         )
     }
 }
+
 export default NewsletterLatest;
