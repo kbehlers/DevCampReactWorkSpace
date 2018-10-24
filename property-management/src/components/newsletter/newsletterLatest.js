eb22c6f86709231daaade4c932da1eb8f5320bdd
kbehlers <kemais.ehlers@gmail.com>
2018-10-24 13:27:06 -0600

    styled newsletter latest and applied row gaps

--- property-management/src/components/newsletter/newsletterLatest.js
+++ property-management/src/components/newsletter/newsletterLatest.js
@@ -6,7 +6,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest__title'>Title goes here</h1>
                 <img src="http://via.placeholder.com/960x258" alt="" className="newsletter-latest__image"/>
                 <div className="newsletter-latest__body">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsum cupiditate, aperiam iure molestias exercitationem accusantium cumque sunt expedita quibusdam totam. Enim doloremque pariatur vero dignissimos, repellat cum atque eos!</p>
 
                 </div>
             </div>
