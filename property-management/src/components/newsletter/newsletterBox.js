6d90ac6bfede5d7db81a158c84e952317a92d129
kbehlers <kemais.ehlers@gmail.com>
2018-10-25 14:19:52 -0600

    fetch and populate date for latest newsletter

--- property-management/src/components/newsletter/newsletterBox.js
+++ property-management/src/components/newsletter/newsletterBox.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default function NewsletterBox({date}){
-    let options = {
-        month: 'short',
-        year: 'numeric'
-    };
-    return(
-        <div className="newsletter-box">
-            <div className='newsletter-box__day'>{date.getDate()}</div>
-            <div className='newsletter-box__month-year'>{date.toLocaleDateString('en-US', options)}</div>
-            <div className="newsletter-box__point"></div>
-        </div>
-    )
+
+class NewsletterBox extends Component{
+    render () {
+        const {date} = this.props;
+        if(!date) {
+            return <div>loading...</div>
+        }
+        let options = {
+            month: 'short',
+            year: 'numeric'
+        };
+        return(
+            <div className="newsletter-box">
+                <div className='newsletter-box__day'>{date.getDate()}</div>
+                <div className='newsletter-box__month-year'>{date.toLocaleDateString('en-US', options)}</div>
+                <div className="newsletter-box__point"></div>
+            </div>
+        )
+    }
 }
 
+function mapStateToProps(state){
+    const {newsletters} =state.newsletters;
+    const latestNewsletter = newsletters[0];
+    return {
+        ...latestNewsletter
+    }
+}
+export default connect(mapStateToProps)(NewsletterBox);
--- property-management/src/components/newsletter/newsletterGrid.js
+++ property-management/src/components/newsletter/newsletterGrid.js
@@ -17,19 +17,13 @@ class NewsletterGrid extends Component {
     }
 
     render() {
-        const latest = {
-            _id: '115',
-            title: 'Happy Holidays',
-            body: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, a! Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsum cupiditate, aperiam iure molestias exercitationem accusantium cumque sunt expedita quibusdam totam. Enim doloremque pariatur vero dignissimos, repellat cum atque eos!`,
-            date: new Date(),
-            imageUrl: 'http://via.placeholder.com/960x258'
-        }
+    
         return (
             <div className='newsletter-grid'>
                 <Button className='newsletter-grid__button' icon='fas fa-plus' callback={()=>this.handleAddNewsletter()}/>
-                <NewsletterBox date={new Date()}/>
+                <NewsletterBox/>
                 <NewsletterArchive/>
-                <NewsletterLatest history={this.props.history} {...latest}/>
+                <NewsletterLatest history={this.props.history}/>
             </div>
         )
     }
