7b1e14a61a4b2ab9acc779127906da4a8fa101ba
kbehlers <kemais.ehlers@gmail.com>
2018-10-26 16:10:14 -0600

    changed styles based on selected requests type.

--- property-management/src/components/requests/requestsBox.js
+++ property-management/src/components/requests/requestsBox.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from '../../actions';
 
 class RequestsBox extends Component{
-    
+
     render () {
 
         const {count, title} = this.props;
-        
+        const className = `requests-box ${this.props.selectedRequestType == title ? 'requests-box-active' : 'requests-box-inactive'}`
         return(
-            <a onClick={(title) => this.props.changeSelectedRequestType(title)} className="requests-box requests-box-inactive">
+            <a onClick={() => this.props.changeSelectedRequestType(title)} className={className}>
                 <div className="requests-box__count">{count}</div>
                 <div className="requests-box__title">{title}</div>
                 <div className="requests-box__point"></div>
@@ -15,5 +17,11 @@ class RequestsBox extends Component{
         )
     }
 }
+
+function mapStateToProps(state) {
+    const {selectedRequestType} = state.requests;
+    return {selectedRequestType};
+}
+RequestsBox = connect(mapStateToProps, actions)(RequestsBox);
 
 export default RequestsBox;
