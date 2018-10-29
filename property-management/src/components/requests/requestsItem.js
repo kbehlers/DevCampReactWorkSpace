bc90c3828e60da2db5d86fa2b26cae01751e3a60
kbehlers <kemais.ehlers@gmail.com>
2018-10-29 09:04:31 -0600

    refactored request item grid

--- property-management/src/components/requests/requestsItem.js
+++ property-management/src/components/requests/requestsItem.js
@@ -8,12 +8,14 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                 <Icon className="requests-item__icon" icon='fas fa-exclamation-triangle'/>
                 <div className="requests-item__title">
-                    My door fell off
+                    <div className='requests-item__title__text'>
+                        My door fell off
+                    </div>
+                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                 </div>
                 <div className="requests-item__tenant-unit">
                     Max - Unit 115
                 </div>
-                <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
                 <div className="requests-item__date">
                     09/15/1997
                 </div>
