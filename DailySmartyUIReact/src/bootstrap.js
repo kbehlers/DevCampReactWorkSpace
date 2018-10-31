6d9acd78c1e1afc1b07370eaffb279bfb9afcdcf
kbehlers <kemais.ehlers@gmail.com>
2018-10-31 11:50:15 -0600

    connected recentposts to redux and set up redux-thunk for api requests

--- DailySmartyUIReact/src/bootstrap.js
+++ DailySmartyUIReact/src/bootstrap.js
@@ -5,8 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
