09e25ee80ba9db916f754ce31d6c8766063d3774
kbehlers <kemais.ehlers@gmail.com>
2018-10-31 14:25:30 -0600

    modified SearchBar Component to take onSubmit function via props

--- DailySmartyUIReact/src/components/home.js
+++ DailySmartyUIReact/src/components/home.js
@@ -5,11 +5,15 @@ import RecentPosts from './recentPosts';
 
 
 export default class Home extends Component {
+  handlleSearchBarSubmit(query) {
+    console.log('trying to handle submit for query', query);
+    this.props.history.push('/results');
+}
   render() {
     return (
       <div className='app'>
         <Logo></Logo>
-        <SearchBar/>
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
         <RecentPosts></RecentPosts>
       </div>
     );
