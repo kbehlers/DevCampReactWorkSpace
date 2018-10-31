e23062fe81f7a6da6a31828fddcebbc8cb92d1c9
kbehlers <kemais.ehlers@gmail.com>
2018-10-31 13:42:49 -0600

    setup redux dev tools and fixed reducer

--- DailySmartyUIReact/src/bootstrap.js
+++ DailySmartyUIReact/src/bootstrap.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import reducers from './reducers';
 import thunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore));
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
