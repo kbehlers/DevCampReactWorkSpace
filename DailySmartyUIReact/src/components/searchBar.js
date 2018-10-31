0075362b6ad7d7dc4addeec06523c88dff09e02a
kbehlers <kemais.ehlers@gmail.com>
2018-10-31 11:00:50 -0600

    recentPosts and searchBar components added

---
+++ DailySmartyUIReact/src/components/searchBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class SearchBar extends Component {
+    render() {
+        return (
+            <form action="">
+                <input type="text" placeholder="Search Daily Smarty" />
+            </form>
+        )
+    }
+}
+export default SearchBar;
