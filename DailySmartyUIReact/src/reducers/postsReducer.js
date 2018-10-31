55e977f8bbd6a712718c89559a341f2888c285b3
kbehlers <kemais.ehlers@gmail.com>
2018-10-31 14:53:01 -0600

    added SET_RESULTS_POSTS reducer

--- DailySmartyUIReact/src/reducers/postsReducer.js
+++ DailySmartyUIReact/src/reducers/postsReducer.js
@@ -1,20 +1,28 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
-export default function(state = INIT_STATE, action) {
+export default function (state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload;
             return {
-                ...state, 
+                ...state,
                 recentPosts
-        }
+            }
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state,
+                resultsPosts
+            }
+
         default:
             return state;
     }
