d16ef7006d2d2786b43da97d9c672edf82b5a701
kbehlers <kemais.ehlers@gmail.com>
2018-11-01 09:54:56 -0600

    added no-content to post links when post links are empty for post

--- DailySmartyUIReact/src/components/post.js
+++ DailySmartyUIReact/src/components/post.js
@@ -45,6 +45,9 @@ class Post extends Component {
             </div>
             )
         })
+        if(links == 0) {
+            return <div className="no-content">No Post Links</div>
+        }
         return links
     }
     render() {
