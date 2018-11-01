907b5d9e6d1a24a42edf50bc04835a980f6063f9
kbehlers <kemais.ehlers@gmail.com>
2018-11-01 16:09:00 -0600

    built form input component and placed email and password fields within signinform component

--- eCommerce/src/components/auth/signinForm.js
+++ eCommerce/src/components/auth/signinForm.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import FormInput from '../formFields';
 class SigninForm extends Component {
     render() {
         const {className} = this.props;
         return (
-            <div className={`${className} sign-in-form`}>
-                
-            </div>
+            <form className={`${className} sign-in-form`}>
+                <Field className='sign-in-form__email'
+                type='email' 
+                title='Email' 
+                placeholder='Email' 
+                name='email' 
+                component={FormInput}/> 
+                <Field className='sign-in-form__password'
+                type='password' 
+                title='Password' 
+                placeholder='Password' 
+                name='password' 
+                component={FormInput}/> 
+            </form>
         )
     }
 }
