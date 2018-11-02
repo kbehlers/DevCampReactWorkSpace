2fe67dd282c158a2e75a998d1a05d0ed7d23ecdf
kbehlers <kemais.ehlers@gmail.com>
2018-11-02 13:31:16 -0600

    styled sign in form and applied grid

--- eCommerce/src/components/layout.js
+++ eCommerce/src/components/layout.js
@@ -6,7 +6,6 @@ class Layout extends Component {
   render() {
     return (
       <div className='layout'>
-        <h1>DevCamp React Starter</h1>
         {this.props.children}
         <Header/>
         <NavBar/>
