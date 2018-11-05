5553e3642ffefbdd9525a0d8524248862d36e305
kbehlers <kemais.ehlers@gmail.com>
2018-11-05 13:58:32 -0700

    account information form started

--- eCommerce/src/components/account/accountInformation.js
+++ eCommerce/src/components/account/accountInformation.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInformationForm';
+
 class AccountInformation extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
     render() {
         return (
-            <div className=''>
-                account info
+            <div className='account-information'>
+                <PageTitle className='account-information' title='Account Information'/>
+                <AccountInformationForm  onSubmit={this.onSubmit} className='account-information__form'/>
             </div>
         )
     }
