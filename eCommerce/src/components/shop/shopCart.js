cca36c8033cef6f52d5a28a0f9c46fc5ff82892a
kbehlers <kemais.ehlers@gmail.com>
2018-11-06 16:09:46 -0700

    added route for order review

--- eCommerce/src/components/shop/shopCart.js
+++ eCommerce/src/components/shop/shopCart.js
@@ -4,6 +4,8 @@ import * as actions from '../../actions';
 import CartProduct from './cartProduct';
 
 import CartButton from './cartButton';
+import history from '../../history';
+
 
 function CartContent({ className, products }) {
     let count = products.length;
@@ -25,7 +27,7 @@ function CartFooter({ className, products }) {
     const price = 7.96;
     return (
         <div className={`${className} cart-footer`}>
-            <a className="cart-footer__checkout">
+            <a onClick={() => history.push('/order/review')} className="cart-footer__checkout">
                 Checkout
             </a>
             <div className="cart-footer__subtotal">
