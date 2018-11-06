fdd73f053440de6488c522feaa303ae82c86b0ce
kbehlers <kemais.ehlers@gmail.com>
2018-11-06 14:34:53 -0700

    mapped cart products into cart product component instances

--- eCommerce/src/components/shop/cartProduct.js
+++ eCommerce/src/components/shop/cartProduct.js
@@ -3,7 +3,9 @@ import Quantity from '../quantity';
 import GreenPriceTag from '../greenPriceTag';
 class CartProduct extends Component {
     render() {
-        const {className, title, quantity, price} = this.props;
+
+        const {className, product, quantity } = this.props;
+        const { title, price } = product;
         return (
             <div className={`${className} cart-product`}>
                 <img src="http://via.placeholder.com/130x130" alt="" className="cart-product__image"/>
