4a61fc9f798a8c563fdc6e333bd6ae6c18e94ae0
kbehlers <kemais.ehlers@gmail.com>
2018-11-07 08:23:11 -0700

    built reviewProducts component. added to grid

--- eCommerce/src/components/order/reviewForm.js
+++ eCommerce/src/components/order/reviewForm.js
@@ -3,13 +3,14 @@ import { reduxForm, Field } from 'redux-form';
 import { FormButton} from '../formFields';
 
 import history from '../../history';
+import ReviewProducts from './reviewProducts';
 
 class ReviewForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit} className={`${className} review-form`}>
-
+                <ReviewProducts className='review-form__products' />
                 <div className="review-form__line"></div>
                 <Field className='review-form__back'
                     onClick={()=> history.push('/shop')}
